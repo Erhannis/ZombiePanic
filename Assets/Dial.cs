72aa22257406e98d2ae9e819ad81f045d05c9034
Erhannis Kirran <eyeillus@gmail.com>
2020-05-23 16:46:02 -0400

    Initial commit - got basic rendering working, started trying to make actual game

---
+++ Assets/Dial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dial : MonoBehaviour
+{
+    public int dots;    
+    public Dial(int dots) {
+        this.dots = dots;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Main.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public ObjectPool pool;
+
+    private List<GameObject> objs = new List<GameObject>();
+    private Random rand = new Random();
+
+    public int targetFPS = 30;
+      
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+      
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.targetFrameRate != targetFPS)
+            Application.targetFrameRate = targetFPS;
+
+        foreach (GameObject o in objs) {
+            pool.PoolObject(o);
+        }
+        objs.Clear();
+        
+        for (int i = 0; i < 10; i++) {
+            GameObject obj = pool.GetObjectForType("Circle", false);
+            obj.transform.position = new Vector3(Random.Range(-4f,4f), Random.Range(-4f,4f), 1);
+            objs.Add(obj);
+        }        
+    }
+}
