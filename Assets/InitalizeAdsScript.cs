55d663c4d1777153cc9552f846c308841a37193f
Erhannis Kirran <eyeillus@gmail.com>
2020-05-30 22:37:12 -0400

    Added ads

---
+++ Assets/InitalizeAdsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour { 
+
+    string gameIdIOS = "3630062"; // iOS
+    string gameIdAndroid = "3630063"; // Android
+    bool testMode = false;
+
+    string placementId = "PlayScreen";
+
+    void Start () {
+        if (Application.platform == RuntimePlatform.IPhonePlayer) {
+            Advertisement.Initialize (gameIdIOS, testMode);
+            StartCoroutine (ShowBannerWhenReady ());
+        } else if (Application.platform == RuntimePlatform.Android) {
+            Advertisement.Initialize (gameIdAndroid, testMode); 
+            StartCoroutine (ShowBannerWhenReady ());
+        }
+    }
+
+    IEnumerator ShowBannerWhenReady () {
+        while (!Advertisement.IsReady (placementId)) {
+            yield return new WaitForSeconds (0.5f);
+        }
+        Advertisement.Banner.Show (placementId);
+    }
+}
---
+++ Assets/InitializeAdsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour { 
+
+    string gameIdIOS = "3630062"; // iOS
+    string gameIdAndroid = "3630063"; // Android
+    bool testMode = true;
+
+    string placementId = "PlayScreen";
+
+    void Start () {
+        if (Application.platform == RuntimePlatform.IPhonePlayer) {
+            Advertisement.Initialize (gameIdIOS, testMode);
+            StartCoroutine (ShowBannerWhenReady ());
+        }
+        if (Application.platform == RuntimePlatform.Android) {
+            Advertisement.Initialize (gameIdAndroid, testMode); 
+            StartCoroutine (ShowBannerWhenReady ());
+        }
+    }
+
+    IEnumerator ShowBannerWhenReady () {
+        while (!Advertisement.IsReady (placementId)) {
+            yield return new WaitForSeconds (0.5f);
+        }
+        Advertisement.Banner.Show (placementId);
+    }
+}
